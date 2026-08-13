e05c692641e8fec899259db98c0eb6e2cf2fc9c4
ytemu <ebnipos@gmail.com>
2026-08-13 23:19:10 +0200

    Implement book scraping and Telegram notificationu
    The script scrapes a website for books with a 5-star rating, saves the data to a CSV file, and sends a message with the file via Telegram.

---
+++ main.py
@@ -0,0 +1,71 @@
+import csv
+import time
+import requests
+from bs4 import BeautifulSoup
+
+TOKEN="8766402164:AAFbfVb9NDeMGIhNXwwjtXRCqWLGO6LxUfc"
+CHAT_ID="8655457768"
+
+while True:
+    books=[]
+
+    print("\n--- STARTING NEW SCRAPING CYCLE ---")
+
+    for page in range(1, 51):
+        url=f"http://books.toscrape.com/catalogue/page-{page}.html"
+        res=requests.get(url)
+        res.encoding="utf-8"
+        soup=BeautifulSoup(res.text, "html.parser")
+
+        products=soup.find_all("article", class_="product_pod")
+
+        for p in products:
+            title=p.find("h3").find("a")["title"]
+            rating=p.find("p", class_="star-rating Five")
+
+            if rating:
+                books.append({"title": title, "rating": "5 Stars"})
+
+    print(f"Scraping completed! Found a total of {len(books)} books.")
+
+    filename="5_star_books.csv"
+
+    with open(filename, mode="w", newline="", encoding="utf-8-sig") as f:
+        writer=csv.DictWriter(f, fieldnames=["title", "rating"])
+        writer.writeheader()
+        writer.writerows(books)
+
+    print(f"Data successfully saved to file: {filename}")
+
+    msg=(
+        f"🚀 **Scraper finished execution!**\n"
+        f"Found a total of **{len(books)}** books with a 5-star rating.\n"
+        f"The complete list is attached in the CSV file below! 👇"
+    )
+
+    text_url=f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    requests.post(
+        text_url,
+        data={"chat_id": CHAT_ID, "text": msg, "parse_mode": "Markdown"},
+    )
+
+    doc_url=f"https://api.telegram.org/bot{TOKEN}/sendDocument"
+
+    with open(filename, "rb") as doc:
+        res=requests.post(
+            doc_url,
+            data={"chat_id": CHAT_ID},
+            files={"document": doc},
+        )
+
+    if res.status_code==200:
+        print("CSV file successfully sent to Telegram! 🎉")
+    else:
+        print("Error sending document:", res.json())
+
+    pause=60
+    print(
+        f"Cycle finished! Script sleeping for {pause} seconds before restarting...\n"
+    )
+
+    time.sleep(pause)u
